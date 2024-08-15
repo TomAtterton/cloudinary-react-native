86982ee9772836d2dcaf9f53db05e4d8ebf0ef19
Tom Atterton <tpatterton@gmail.com>
2024-08-15 11:53:37 +0200

    Remove Video / Image

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,2 @@
-import AdvancedImage from "./AdvancedImage";
-import AdvancedVideo from './AdvancedVideo';
-export { upload, unsignedUpload, uploadBase64, rename, explicit } from "./api/upload";
+export { upload, unsignedUpload, uploadBase64, rename, explicit } from './api/upload';
 export { UploadApiOptions } from './api/upload/model/params/upload-params';
-
-export { AdvancedImage };
-export { AdvancedVideo };
